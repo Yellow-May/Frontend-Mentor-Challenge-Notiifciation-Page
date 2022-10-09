65c8e1b1f963d89c1187a6ca760b9710dc33b33f
yellow_may <yellow.may.21@gmail.com>
2022-10-09 18:46:08 +0100

    project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      red: 'hsl(1, 90%, 64%)',
+      blue: 'hsl(219, 85%, 26%)',
+      white: 'hsl(0, 0%, 100%)',
+      'grayish-blue': {
+        DEFAULT: 'hsl(219, 14%, 63%)',
+        50: 'hsl(210, 60%, 98%)',
+        100: 'hsl(211, 68%, 94%)',
+        200: 'hsl(205, 33%, 90%)',
+        300: 'hsl(219, 12%, 42%)',
+        400: 'hsl(224, 21%, 14%)'
+      }
+    },
+    extend: {
+      fontFamily: {
+        main: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
